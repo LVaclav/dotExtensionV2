517e33e801e045b68b9d43b478b98fc0f7ddb6a1
Marshamallow37 <Liam@cervenka.space>
2023-05-13 12:36:20 +0800

    GoGo

--- content.js
+++ content.js
@@ -1,48 +1,131 @@
 console.log('From content: script loaded');
 
 if (document.title === 'PDA Bookings') {
-  const parentElement = document.querySelector('#id7 > div.licensing-big-form');
+  const parentElement = $('td.ct-active');
   const menuTemplate = `
-    <div id="MyExtension" style="border-style: solid; border-width: 4px;">
-      <input type="checkbox" value="ROCK">
-      <label> Book the test (if not it will jsut alert you) </label>
-      <input type="submit" value="Ahoj!!" class="licensing-button-short">
+    <div class="container" id="MyExtension" style="border-style: solid; border-width: 4px">
+    <h3>DOT Automatic Test Booker</h3>
+    <h5 id="status" style="font-style: italic; color: red;" >Not Running</h5>
+      <ol>
+        <li>
+          <span class="emphasized-plain">Book or Alert</span>
+          <input id="bookSwitch" type="checkbox" checked="cheked" value="ROCK" />
+          <label id="bookLabel"> Book </label>
+        </li>
+        <br/>
+
+       <!--
+
+        <li>
+          <span class="emphasized-plain">Earliest Start Time</span>
+          <input type="time" />
+          <label> Earliest </label>
+        </li>
+
+        <li>
+          <span class="emphasized-plain">Latest Start Time</span>
+          <input type="time" />
+          <label> Latest </label>
+          
+        </li>
+
+        <li><span style="font-style: italic;">(Leave blank if you have no time prefernece. Will get test quicker)</span></li>
+
+        --!>
+
+        <li style="text-align: right">
+          <input type="button" value="Start Looking!!" id="StartLooking" class="licensing-button-short" />
+          <input type="button" value="Stop Search" id="StopLooking" class="licensing-button-short" />
+        </li>
+      </ol>
     </div>
   `;
   parentElement.insertAdjacentHTML('beforeend', menuTemplate);
-}
 
-console.log(name);
-function RunScript() {
-  myIntervel = setInterval(function () {
-    var now = new Date();
-    var current_time = now.toLocaleTimeString();
-    var current_date = now.toLocaleDateString();
-    console.log('script running' + current_time);
-    // try {
-    //   //change 'id8' to 'idb' if you are using the change booking interface
-    //   document.getElementById('id8').click();
-    //   let state = document.getElementsByClassName('feedbackPanelERROR');
-    //   let stateParsed = state[0].innerText;
-    //   var noBook =
-    //     'Sorry, there are no bookings available for the date requested. Please alter your requested booking date and try again.';
-    //   console.log('nada ' + current_time);
-    // } catch (err) {
-    //   let booking = document.querySelectorAll('#searchResultRadioLabel');
-    //   console.log('These results appeared at ' + current_time + ' :');
-    //   for (let i = 0; i < booking.length; i++) {
-    //     console.log(booking[i].innerHTML);
+  //Alert Or booking Settings
+  var checkbox = document.getElementById('bookSwitch');
+  var label = document.getElementById('bookLabel');
+  checkbox.onclick = function updateLabel() {
+    // Check if the checkbox is checked
+    if (checkbox.checked) {
+      // If the checkbox is checked, set the label text to "Book"
+      label.textContent = 'Book Test';
+    } else {
+      // If the checkbox is not checked, set the label text to "Alert"
+      label.textContent = 'Alert ';
+    }
+  };
 
-    //     //define components
-    //     const components = booking[i].innerHTML.split(' at ');
-    //     const booking_date = components[0];
-    //     const timeAndLocation = components[1];
-    //     const booking_time_12h =
-    //       timeAndLocation.split(' ')[1] + ' ' + timeAndLocation.split(' ')[2];
-    //     const booking_location = timeAndLocation.split(' ').slice(3).join(' ');
-    //     [];
-    //   }
-    // }
-  }, 20000);
-  x;
+  //Script
+
+  // Start Search
+  var searchButton = document.getElementById('StartLooking');
+  searchButton.onclick = function RunScript() {
+    var status = (document.getElementById('status').innerHTML = ' Running');
+    var status = (document.getElementById('status').style =
+      'font-style: italic; color: green;');
+
+    //Search Loop
+    myIntervel = setInterval(function () {
+      try {
+        $('div#mnbbuttonBar>table>tbody>tr>td').click();
+        var now = new Date(); //date
+        var current_time = now.toLocaleTimeString();
+        var current_date = now.toLocaleDateString();
+        let state = document.getElementsByClassName('feedbackPanelERROR');
+        let stateParsed = state[0].innerText;
+        var noBook =
+          'Sorry, there are no bookings available for the date requested. Please alter your requested booking date and try again.';
+        console.log('nada ' + current_time);
+      } catch (err) {
+        let booking = document.querySelectorAll('#searchResultRadioLabel');
+        for (let i = 0; i < booking.length; i++) {
+          console.log(booking[i].innerHTML);
+
+          //define components
+          const components = booking[i].innerHTML.split(' at ');
+          const booking_date = components[0];
+          const timeAndLocation = components[1];
+          const booking_time_12h =
+            timeAndLocation.split(' ')[1] + ' ' + timeAndLocation.split(' ')[2];
+          const booking_location = timeAndLocation
+            .split(' ')
+            .slice(3)
+            .join(' ');
+          [];
+
+          if (checkbox.checked) {
+            $('input#searchResultRadio0').click();
+            $('input#id3e').click();
+            clearInterval(myIntervel);
+            alert(
+              'test booked :) for:' +
+                booking_date +
+                ' ' +
+                booking_time_12h +
+                ' at ' +
+                booking_location
+            );
+          } else {
+            alert(
+              'A Test Is available: ' +
+                booking_date +
+                ' ' +
+                booking_time_12h +
+                ' at ' +
+                booking_location
+            );
+          }
+        }
+      }
+    }, 5000);
+  };
+  // Stop Search
+  var stopSearch = document.getElementById('StopLooking');
+  stopSearch.onclick = function stopSearch() {
+    clearInterval(myIntervel);
+    var status = (document.getElementById('status').innerHTML = 'Not Running');
+    var status = (document.getElementById('status').style =
+      'font-style: italic; color: red;');
+  };
 }
